03831f1ed96a754bd4b6ac5d0ce7d84886365498
pcheng11 <pcheng11@32a8a77e-3dd4-4e5b-b375-dd4463c9af65>
2017-02-16 07:22:00 +0000

    L

--- shell.c
+++ shell.c
@@ -15,10 +15,36 @@ typedef struct process {
   pid_t pid;
 } process;
 
+void *string_copy_constructor(void *elem) {
+  char *str = elem;
+  assert(str);
+  return strdup(str);
+}
+
+// This is the destructor function for string element.
+// Use this as destructor callback in vector.
+void string_destructor(void *elem) { free(elem); }
+
+void *string_default_constructor(void) {
+  // A single null byte
+  return calloc(1, sizeof(char));
+}
+
 int shell(int argc, char *argv[]) {
   // TODO: This is the entry point for your shell.
 	//input is not a file;
+
+	//create a status tracker
+	
+	vector *com = vector_create(string_copy_constructor, string_destructor, string_destructor);
+	
+	vector *stat = vector_create(string_copy_constructor, string_destructor, string_destructor);
+
+	vector *p = vector_create(int_copy_constructor, int_destructor, int_destructor);
+	
+
 	int exit_ = 0;
+
 	while(exit_ == 0)
   {
 
@@ -32,6 +58,15 @@ int shell(int argc, char *argv[]) {
 		char *directory = getcwd(NULL, 0);
 		print_prompt(directory, main_pro);
 		free(directory);
+		//stock process
+			process *a;
+			a->command = argv[0];
+			vector_push_back(com, a->command);
+			a.pid = (int)main_pro;
+			vector_push_back(com, (int)a.pid);
+			//稍后需要判断
+			a->status = STATUS_RUNNING;
+			vector_push_back(com, (int)a->pid);
 		//get stdin
 		char *buffer = NULL;
 		size_t size = 0;
@@ -39,13 +74,23 @@ int shell(int argc, char *argv[]) {
 		//see what is the command
 		if(strcmp(buffer, "cd\n") == 0)
 			print_no_directory("");
+
+			
+
+		if(strcmp(buffer, "ps\n") == 0)
+		{
+			void print_process_info(vector_front(stat), vector_font(p), vector_front(com));
+
+		}
 		else
 		{
 			char *temp_dir = strdup(buffer + 3);
 			size_t a = strlen(temp_dir);
 			temp_dir[a-1] = '\0';
 			//printf("%s", temp_dir);
+
 			// dir does not exist
+
 			if(opendir(temp_dir)== NULL)
 			{
 				print_no_directory(temp_dir);
