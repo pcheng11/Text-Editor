5e44b77579488031a48666bec59806345cf53ab1
pcheng11 <pcheng11@32a8a77e-3dd4-4e5b-b375-dd4463c9af65>
2017-02-17 03:31:38 +0000

    L

--- pcheng11/shell/shell.c
+++ pcheng11/shell/shell.c
@@ -38,7 +38,8 @@ int shell(int argc, char *argv[]) {
   // TODO: This is the entry point for your shell.
 	//input is not a file;
 
-	
+	if(argc > 3 || argc == 2 || (argc == 3 && getopt(argc, argv, "f") != -1))
+		print_usage();
 	
 // create 3 vectors to keep track of process info
 	vector *pid_info = int_vector_create();
