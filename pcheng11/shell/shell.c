f0c540e53075174c70d9d6a174ef51b2c71ba304
pcheng11 <pcheng11@32a8a77e-3dd4-4e5b-b375-dd4463c9af65>
2017-02-18 03:19:39 +0000

    L

--- pcheng11/shell/shell.c
+++ pcheng11/shell/shell.c
@@ -272,7 +272,7 @@ int shell(int argc, char *argv[]) {
   					print_fork_failed();
   				if (child == 0) 
   				{ /* I have a child! */
-    				execvp(buffer, &buffer);
+    				execvp(tell, &buffer);
     			
     				print_exec_failed(buffer);
     				break;
