69a5403d93d33177759fff46fb08b5322851a217
pcheng11 <pcheng11@32a8a77e-3dd4-4e5b-b375-dd4463c9af65>
2017-02-18 04:36:52 +0000

    L

--- shell.c
+++ shell.c
@@ -295,7 +295,7 @@ int shell(int argc, char *argv[]) {
   				if (child == 0) 
   				{ /* I have a child! */
     				printf("%s\n", buffer);
-    				execvp(buffer, &temp_dir);
+    				execlp(buffer, &temp_dir, (char*)NULL);
     				print_exec_failed(buffer);
     				break;
    				} 
