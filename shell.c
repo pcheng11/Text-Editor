a60220096f31cf797e510da0ab8e0d3c2e8fee04
pcheng11 <pcheng11@32a8a77e-3dd4-4e5b-b375-dd4463c9af65>
2017-02-18 20:23:04 +0000

    L

--- shell.c
+++ shell.c
@@ -276,7 +276,7 @@ int shell(int argc, char *argv[]) {
 					token_array = strsplit(buffer, " \n", &num_tokens);
 					buffer[len-1]= '\0';
 					signal(SIGCHLD, cleanup);
-				
+					
 				 	child_b = fork();
 			 		
 			 		
@@ -307,6 +307,7 @@ int shell(int argc, char *argv[]) {
 
 				else
 				{
+					fflush(stdout);
 				 	 child = fork();
 				 		process b;
 					 buffer[len-1]= '\0';
